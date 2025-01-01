d11c7c6b95c25b44f527a80b1f82c61e51af6ce1
PHNPR <praneethreddyphn@gmail.com>
2025-01-01 21:49:48 +0530

    POTD -> Happy New Year

---
+++ 1422.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        n, l, r, ans = len(s), 0, s.count('1'), 0
+
+        for i in range(n - 1) :
+            if s[i] == '0' :
+                l += 1 
+            else :
+                r -= 1 
+            ans = max(ans, l + r)
+        
+        return ans
