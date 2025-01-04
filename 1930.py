f1ddee0f8be99ca1fe72b6994b6aa8f5b40f8dcf
PHNPR <praneethreddyphn@gmail.com>
2025-01-05 00:42:12 +0530

    POTD 5/1/24

---
+++ 1930.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        ans = 0 
+        for i in range(97, 123) :
+            try :
+                l, r = s.index(chr(i)), s.rindex(chr(i))
+                ans += len(set(s[l+1:r]))
+            except :
+                continue 
+        return ans 
