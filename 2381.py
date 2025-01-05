e0f2484626e7df202a14a9920ae80bafa9f4754e
PHNPR <praneethreddyphn@gmail.com>
2025-01-05 09:19:40 +0530

    POTD->5/1/24->Shifting Letter II

---
+++ 2381.py
@@ -0,0 +1,19 @@
+from typing import List 
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        dp = [0] * (len(s) + 1)
+        
+        for a , b , c in shifts :
+            dp[a] += (2*c - 1) 
+            dp[b + 1] -= (2*c - 1)
+        
+        f = lambda i : chr(97 + (ord(s[i]) - 97 + dp[i]) % 26)
+
+        val = f(0)
+
+        for i in range(1, len(s)) :
+            dp[i] += dp[i - 1]
+            val += f(i)
+        
+        return val
